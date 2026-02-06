5d2171e897aef06370e8b01fb7ff3fdfed3c743e
bugblazer <daregamer2004@gmail.com>
2026-02-06 23:22:30 +0500

    Created the states for the client side, refactored main.gd script and shifted the logic into respective states

---
+++ client/game_manager.gd
@@ -0,0 +1,27 @@
+extends Node
+
+#A list of states:
+enum State {
+	ENTERED, #Connecting to the server etc
+	INGAME, #In-game logic, chat etc
+}
+
+#Creating a dictionary that will hold scenes related to different states
+var _states_scenes: Dictionary[State, String] = {
+	State.ENTERED: "res://states/entered/entered.tscn",
+	State.INGAME: "res://states/ingame/ingame.tscn",
+}
+
+var client_id: int
+var _current_scene_root: Node
+
+#Sets the scene according to the state and deletes the previous one from queue
+func set_state(state: State) -> void:
+	if _current_scene_root != null:
+		_current_scene_root.queue_free()
+		
+	#Grabs the path from the enum and loads it
+	var scene: PackedScene = load(_states_scenes[state])
+	_current_scene_root = scene.instantiate()
+	add_child(_current_scene_root)
+	
